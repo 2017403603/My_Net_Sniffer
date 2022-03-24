fcc0d42d2dcd144d8c82404a0e6ff6c5f70727d9
cgg <705328646@qq.com>
2022-03-25 00:08:56 +0800

    MVC

---
+++ src/com/company/Model/demo.java
@@ -0,0 +1,15 @@
+package com.company.Model;
+
+/**
+ * @ClassName demo  //类名称
+ * @Description: 类描述
+ * @Author: 程哥哥    //作者
+ * @CreateDate: 2022/3/25 0:02	//创建时间
+ * @UpdateUser: 更新人
+ * @UpdateDate: 2022/3/25 0:02	//更新时间
+ * @UpdateRemark: 更新的信息
+ * @Version: 1.0    //版本号
+ */
+
+public class demo {
+}
