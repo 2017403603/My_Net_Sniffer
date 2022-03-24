71589e4f2efef08156895cca625a6bf93b1599b1
cgg <705328646@qq.com>
2022-03-24 23:54:09 +0800

    MVC

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
-import java.io.IOException;
-
-import jpcap.*;
-import jpcap.packet.IPPacket;
-import jpcap.packet.Packet;
+/**
+ * @ClassName Main  //类名称
+ * @Description: 类描述 //程序入口
+ * @Author: 程哥哥    //作者
+ * @CreateDate: 2022/3/24 23:12	//创建时间
+ * @UpdateUser: 更新人
+ * @UpdateDate: 2022/3/24 23:12	//更新时间
+ * @UpdateRemark: 更新的信息
+ * @Version: 1.0    //版本号
+ */
 public class Main {
 
     public static void main(String[] args) {
